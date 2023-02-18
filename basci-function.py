f5f2774d35ea78da907a5cc41bf2801732cfdc80
BE LAB <63097527+Thawach@users.noreply.github.com>
2023-02-18 11:56:41 +0700

    Basic Function

---
+++ basci-function.py
@@ -0,0 +1,24 @@
+def hello():
+    print('Hello my friend')
+
+def sawadee():
+    print('sawadee')
+
+def nihao():
+    print('nihao')
+
+def konichiwa():
+    print('konichiwa')
+
+
+while True:
+    friend = input('Where are you from: ')
+
+    if friend =='thai':
+        sawadee()
+    elif friend =='china':
+        nihao()
+    elif friend =='japan':
+        konichiwa()
+    else:
+        hello()
